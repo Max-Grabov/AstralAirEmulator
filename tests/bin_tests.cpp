f46a007f7a8b1a8be444f70bb5e768987cf2a64c
Max-Grabov <maxpgrabov@gmail.com>
2026-05-19 20:09:03 -0400

    chore: test

--- tests/bin_tests.cpp
+++ tests/bin_tests.cpp
@@ -22,3 +22,17 @@ TEST(BinTest, TestVoiceOpen)
     EXPECT_EQ(static_cast<char>(data[0].GetName()[it]), comparison[it]);
   }
 }
+
+TEST(BinTest, TestDataRead)
+{
+  using AstralAir::Data::AstralAirData;
+  using AstralAir::Formats::BinFormat;
+  using AstralAir::Formats::View;
+
+  BinFormat bin("./AstralAirData/voice.bin");
+  View data_view("./AstralAirData/voice.bin");
+
+  std::vector<AstralAirData> data{bin.OpenAndRead()};
+
+  std::cout << reinterpret_cast<const char *>(data_view.Read(data[0].GetOffset(), data[0].GetData()).data()) << "\n";
+}
