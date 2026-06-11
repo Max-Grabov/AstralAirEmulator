9d019260c957c1c5d13581a0ff311f831c0e48cc
Max-Grabov <maxpgrabov@gmail.com>
2026-06-11 19:33:25 -0400

    chore: last image

--- tests/image_tests.cpp
+++ tests/image_tests.cpp
@@ -41,11 +41,14 @@ TEST(HZC_StreamTest, TestGetImage)
   bg_bin.OpenAndRead();
   BinFormat bs_bin("./AstralAirData/graph_bs.bin");
   bs_bin.OpenAndRead();
+  BinFormat g_bin("./AstralAirData/graph.bin");
+  g_bin.OpenAndRead();
   
   View vis_view("./AstralAirData/graph_vis.bin");
   View sd_view("./AstralAirData/graph_sd.bin");
   View bg_view("./AstralAirData/graph_bg.bin");
   View bs_view("./AstralAirData/graph_bs.bin");
+  View g_view("./AstralAirData/graph.bin");
 
   std::vector<std::byte> vis_query =
       vis_view.Read(8 + vis_view.Read<uint32_t>(0) * 12 + vis_view.Read<uint32_t>(8), 9);
@@ -55,19 +58,24 @@ TEST(HZC_StreamTest, TestGetImage)
       bg_view.Read(8 + bg_view.Read<uint32_t>(0) * 12 + bg_view.Read<uint32_t>(8), 9);
   std::vector<std::byte> bs_query =
       bs_view.Read(8 + bs_view.Read<uint32_t>(0) * 12 + bs_view.Read<uint32_t>(8), 17);
+  std::vector<std::byte> g_query =
+      g_view.Read(8 + g_view.Read<uint32_t>(0) * 12 + g_view.Read<uint32_t>(8), 8);
   
   std::vector<std::byte> vis_data = vis_bin.GetChunk(vis_query);
   std::vector<std::byte> sd_data = sd_bin.GetChunk(sd_query);
   std::vector<std::byte> bg_data = bg_bin.GetChunk(bg_query);
   std::vector<std::byte> bs_data = bs_bin.GetChunk(bs_query);
+  std::vector<std::byte> g_data = g_bin.GetChunk(g_query);
 
   std::optional<HZC_Stream> vis_hzc = HZC_Stream::Construct(std::move(vis_data)); 
   std::optional<HZC_Stream> sd_hzc = HZC_Stream::Construct(std::move(sd_data));
   std::optional<HZC_Stream> bg_hzc = HZC_Stream::Construct(std::move(bg_data));
   std::optional<HZC_Stream> bs_hzc = HZC_Stream::Construct(std::move(bs_data));
+  std::optional<HZC_Stream> g_hzc = HZC_Stream::Construct(std::move(g_data));
 
   EXPECT_TRUE(vis_hzc);
   EXPECT_TRUE(sd_hzc);
   EXPECT_TRUE(bg_hzc);
   EXPECT_TRUE(bs_hzc);
+  EXPECT_TRUE(g_hzc);
 }
